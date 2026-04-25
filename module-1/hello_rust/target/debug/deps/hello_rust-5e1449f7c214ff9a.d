8c1c44f10c99d8937c90adbb5a67a0144dfd5b0d
Kurnia Massidik <k.massidik@gmail.com>
2026-04-25 19:55:44 +0700

    fix: remove target folder from tracking

--- module-1/hello_rust/target/debug/deps/hello_rust-5e1449f7c214ff9a.d
+++
@@ -1,5 +0,0 @@
-/Users/mac/Desktop/dev-envs/rust/Learning-Rust/module-1/hello_rust/target/debug/deps/hello_rust-5e1449f7c214ff9a.d: src/main.rs
-
-/Users/mac/Desktop/dev-envs/rust/Learning-Rust/module-1/hello_rust/target/debug/deps/hello_rust-5e1449f7c214ff9a: src/main.rs
-
-src/main.rs:
--- module-1/hello_rust/target/debug/hello_rust.d
+++
@@ -1,1 +0,0 @@
-/Users/mac/Desktop/dev-envs/rust/Learning-Rust/module-1/hello_rust/target/debug/hello_rust: /Users/mac/Desktop/dev-envs/rust/Learning-Rust/module-1/hello_rust/src/main.rs
